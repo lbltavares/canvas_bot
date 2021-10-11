30b46d345d9b5e6b96b2890d8f7fbefd7c2a23e0
lucas <lucas.laborne@gmail.com>
2021-10-11 08:52:23 -0300

    Implementa o metodo proximas()

--- api.py
+++ api.py
@@ -1,13 +1,27 @@
 import cache
-from datetime import datetime, timedelta, timezone
+from datetime import datetime as dt, timezone as tz
 
 
 def calendario():
     pass
 
 
-def proximas(limit=1):
-    return []
+def proximas(limit=None):
+    quizzes = cache.get_quizzes()
+    assigns = cache.get_assignments()
+    courses = cache.get_courses(as_dict=True)
+    result = []
+    for t in quizzes + assigns:
+        c = courses[t.course_id]
+        due_at = t.due_at
+        if due_at is None:
+            continue
+        due_at_date = t.due_at_date
+        agora = dt.now().astimezone()
+        if due_at_date > agora:
+            result.append((c.name, t))
+    result = sorted(result, key=lambda x: x[1].due_at_date)
+    return result[:limit]
 
 
 def all():
